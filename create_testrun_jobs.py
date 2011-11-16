82fe926152b19356180e09a4f993dea5814eeb01
Dave Hunt <dave.hunt@gmail.com>
2011-11-16 19:31:56 +0000

    Initial commit.

---
+++ create_testrun_jobs.py
@@ -0,0 +1,73 @@
+import os
+import xml.dom.minidom
+
+import jenkins
+
+testruns = (
+    {'type': 'functional'},
+    {'type': 'update',
+     'options': ['--no-fallback']},
+    {'type': 'l10n'},
+    {'type': 'endurance',
+     'options': ['--iterations=100', '--entities=10']},
+    {'type': 'remote'},
+    {'type': 'addons',
+     'options': ['--with-untrusted']})
+
+nodes = (
+    {'labels': ['windows', '2000'],
+     'platforms': ['win32'],
+     'environment': 'windows'},
+    {'labels': ['windows', 'xp'],
+     'platforms': ['win32'],
+     'environment': 'windows'},
+    {'labels': ['windows', 'vista'],
+     'platforms': ['win32'],
+     'environment': 'windows'},
+    {'labels': ['windows', '7'],
+     'platforms': ['win32'],
+     'environment': 'windows'},
+    {'labels': ['windows', '7x64'],
+     'platforms': ['win32', 'win64'],
+     'environment': 'windows'},
+    {'labels': ['macos', '10.6.8x64'],
+     'platforms': ['mac', 'mac64'],
+     'environment': 'mac'},
+    {'labels': ['ubuntu', '10.10'],
+     'platforms': ['linux'],
+     'environment': 'linux'},
+    {'labels': ['ubuntu', '10.10x64'],
+     'platforms': ['linux', 'linux64'],
+     'environment': 'linux'})
+
+testrun_command = '%(script)s ./testrun_%(type)s.py --port=2424${EXECUTOR_NUMBER} --junit=results.xml --logfile=%(type)s.log --report=http://mozmill-archive.brasstacks.mozilla.com/db/ %(options)s .'
+
+def main():
+    j = jenkins.Jenkins('http://localhost:8080')
+    count = 1
+    for testrun in testruns:
+        type = testrun.get('type')
+        for node in nodes:
+            labels = node.get('labels')
+            job_name = '-'.join([type, '-'.join(labels)])
+            print '%i: %s' % (count, job_name)
+            count = count + 1
+            template = open(os.path.join('templates', 'testrun_job.xml'))
+            platforms = ''.join(['<string>%s</string>' % platform for platform in node.get('platforms')])
+            command = testrun_command % {
+                'script': './mozmill-env/run.sh',
+                'type': type,
+                'options': 'options' in testrun and ' '.join(testrun.get('options')) or ''
+            }
+            doc = xml.dom.minidom.parseString(template.read() % {
+                'labels': '&amp;&amp;'.join(labels),
+                'environment': node.get('environment'),
+                'platforms': platforms,
+                'testrun_command': command})
+            if j.job_exists(job_name):
+                j.reconfig_job(job_name, doc.toxml())
+            else:
+                j.create_job(job_name, doc.toxml())
+
+if __name__ == '__main__':
+    main()
---
+++ trigger_on_demand_testrun.py
@@ -0,0 +1,93 @@
+""" Script to create and trigger Firefox testruns in Jenkins. """
+
+import ConfigParser
+import os
+import re
+import sys
+import uuid
+import xml.dom.minidom
+
+import jenkins
+
+
+def main():
+    j = jenkins.Jenkins('http://localhost:8080')
+
+    if not len(sys.argv) == 2:
+        print 'Configuration file not specified.'
+        print 'Usage: %s config' % sys.argv[0]
+        sys.exit(1)
+
+    # Read-in configuration options
+    config = ConfigParser.SafeConfigParser()
+    config.read(sys.argv[1])
+
+    # Check testrun entries
+    testrun = { }
+    for entry in config.options('testrun'):
+        testrun.update({entry: config.get('testrun', entry)})
+    testrun = testrun
+
+    # Define job and triggers
+    job_name = 'ondemand-%s-%s' % (testrun['script'], uuid.uuid4())
+    triggers = [ ]
+
+    # Iterate through all target nodes
+    for section in config.sections():
+        # Retrieve the platform, i.e. win32 or linux64
+        if not config.has_option(section, 'platform'):
+            continue
+        platform = config.get(section, 'platform')
+        node_labels = section.split(' ')
+        sub_job_name = '%s-%s' % (testrun['script'], '-'.join(node_labels))
+
+        # Iterate through all builds per platform
+        for entry in config.options(section):
+            locales = [ ]
+            build_type = 'release'
+
+            # Expression to parse versions like: '5.0', '5.0#3', '5.0b1', '5.0b2#1'
+            pattern = re.compile(r'(?P<version>[\d\.]+(?:\w\d+)?)(?:#(?P<build>\d+))?')
+            try:
+                (version, build) = pattern.match(entry).group('version', 'build')
+                locales = config.get(section, entry).split(' ')
+
+                # If a build number has been specified we have a candidate build
+                build_type = 'candidate' if build else build_type
+            except:
+                continue
+
+            # Create triggered job for each listed locale
+            for locale in locales:
+                triggers.append({
+                    'job_name': sub_job_name,
+                    'parameters': {
+                        'PLATFORM': platform,
+                        'BUILD_TYPE': build_type,
+                        'VERSION': version,
+                        'BUILD': build or '1',
+                        'LOCALE': locale
+                    }
+                })
+
+    # Create parameterized build triggers
+    pbt_template = open(os.path.join('templates', 'parameterized_build_trigger_config.xml')).read()
+    pbts = [ ]
+    for trigger in triggers:
+        pbts.append(pbt_template % {
+            'job_name': trigger['job_name'],
+            'parameters': '\n'.join(['%s=%s' % (k, v) for k, v in trigger['parameters'].items()])})
+
+    # Create on-demand-job
+    job_template = open(os.path.join('templates', 'on_demand_job.xml')).read()
+    job_config = xml.dom.minidom.parseString(job_template % {
+        'parameterized_build_trigger_config': '\n'.join(pbts)})
+    print 'Creating job: %s' % job_name
+    j.create_job(job_name, job_config.toxml())
+
+    # Trigger on-demand job
+    print 'Building job: %s' % job_name
+    j.build_job(job_name)
+
+if __name__ == "__main__":
+    main()
